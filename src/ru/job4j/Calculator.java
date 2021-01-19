63a16e265b2079758b8c684f319224a146a1a7ff
USER <vidinvitalii@gmail.com>
2021-01-19 13:22:52 +0200

    новый класс Calculator 19012021 13 22

--- src/ru/job4j/Calculator.java
+++ src/ru/job4j/Calculator.java
@@ -1,15 +1,15 @@
-package ru.job4j.calculator;
+package ru.job4j;
 
 public class Calculator {
-    public static void main(String[] args){
+    public static void main(String[] ange) {
         int one = 1;
         int two = 2;
         int six = 6;
         int four = 4;
         int five = 5;
-        int sixDivTwo = six/two;
-        int fiveMinusTwo = five-two;
-        int fourTimeTwo = four*two;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
         int onePlusTwo = one + two;
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
