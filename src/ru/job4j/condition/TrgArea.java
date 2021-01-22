88d2a1d9d2fe1b900bc64a7691402d555b7fe087
USER <vidinvitalii@gmail.com>
2021-01-22 21:43:32 +0200

    новый класс TrgArea 22 01 2021   21  43

---
+++ src/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class TrgArea {
+    public static double area(double a,double b,double c){
+        double p = 0.5 * (a+b+c);
+        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    }
+    public static void main(String[] args){
+        double rsl = TrgArea.area(2,2,2);
+        System.out.println("area (2, 2, 2) = " + rsl);
+    }
+}
