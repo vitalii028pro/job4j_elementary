c9ed151434eefea6af76c0955c78ed2600e36eae
USER <vidinvitalii@gmail.com>
2021-01-22 22:05:57 +0200

    новый класс Greeting 22 01 2021   22 05

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args){
+        int year = 2021;
+        String idea = "I like java! " + "But I am a newbie. " + year;
+        System.out.println(idea);
+    }
+}
