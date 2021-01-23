1266a47717840c882cc41ef3d65bdbb62d85c810
USER <vidinvitalii@gmail.com>
2021-01-23 22:09:23 +0200

    новый класс SqArea 23 01 2021   23 09

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,15 +1,13 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double height(double p,double k){
-          return p / (2 * (k + 1));
+    public static double squrea(double p,double k){
+       double h = p / (2 * (k + 1));
+        double l = h * k;
+          return l * h;
     }
         public static void main(String[] args){
-        double p = 6;
-        double k = 2;
-        double resultH = SqArea.height(p,k);
-        double resultL = resultH*k;
-        double resultS = resultH*resultL;
-        System.out.println(" p = 6, k = 2, s = 2, real =   " + "Longer = " + resultL + "   height = " + resultH);
+           double result1 = SqArea.squrea(6,2);
+        System.out.println(" p = 6, k = 2, s = 2, real =   " + result1);
     }
 }
