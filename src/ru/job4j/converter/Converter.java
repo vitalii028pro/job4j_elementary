1af66a7c7ec9cded22a14f98099ce8bdbe1fb9e0
USER <vidinvitalii@gmail.com>
2021-01-25 20:43:24 +0200

    тест метода rubleToDollar

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,17 +2,25 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static int a(int a1){
-            return a1/77;
+    public static int rubleToEuro(int value) {
+        return value / 70;
     }
-    public static int b(int b1){
-                return b1/60;
+
+    public static int rubleToDollar(int value) {
+        return -1;
     }
-    public static void main(String[] args){
-        int euro  = Converter.a(100);
-        int doll = Converter.b(100);
-        System.out.println("154 rubles are " + euro + " euro ");
-        System.out.println("120 rubles are " + doll + " doll ");
+
+    public static void main(String[] args) {
+        int in = 140;
+        int in1 = 10;
+        int expected = 2;
+        int expected1 = -1;
+        int out = Converter.rubleToEuro(in);
+        int out1 = Converter.rubleToDollar(in1);
+        boolean passed = expected == out;
+        boolean passed1 = expected1 == out1;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        System.out.println("Любое число. Test result : " + passed1);
 
     }
 }
