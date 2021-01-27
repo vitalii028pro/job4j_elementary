2fd0b8fd592ec5fe805291065efafe6854722a43
USER <vidinvitalii@gmail.com>
2021-01-27 18:12:58 +0200

    new test for SqArea.square

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,13 +1,15 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double squrea(double p,double k){
+    public static double square(double p,double k){
        double h = p / (2 * (k + 1));
          double l = h * k;
           return l * h;
     }
         public static void main(String[] args){
-           double result1 = SqArea.squrea(6,2);
+           double result1 = SqArea.square(6,2);
+           double result2 = SqArea.square(10,3);
         System.out.println(" p = 6, k = 2, s = 2, real =   " + result1);
+        System.out.println(" p = 10, k = 3   real =   " + result2);
     }
 }
---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SqAreaTest {
+
+    @Test
+    public void squareTest() {
+        int expected = 2;
+        int p = 6;
+        int k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+    public void squareTest1(){
+        int expected = 4;
+        int p = 10;
+        int k = 3;
+        double out = SqArea.square(p,k);
+        Assert.assertEquals(expected,out,0.69);
+    }
+}
