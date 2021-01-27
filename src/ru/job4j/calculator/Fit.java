60c485d3dc597a75e631de4829f7ea254a693924
USER <vidinvitalii@gmail.com>
2021-01-27 17:18:19 +0200

    Test for class Fit

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,10 +2,11 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height){
-                 return (height-100)*1.15;
+        return (height-100)*1.15;
     }
     public static double womenWeight(short height){
-                return (height-110)*1.15;
+
+        return (height-110)*1.15;
     }
     public static void main(String[] args){
             short height = 187;
