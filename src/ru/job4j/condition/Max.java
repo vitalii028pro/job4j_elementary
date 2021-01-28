7970e28b82b29dfa6f67cfa81f33b6b6201a1acc
USER <vidinvitalii@gmail.com>
2021-01-28 19:42:30 +0200

    new class Max and MaxTest

---
+++ src/ru/job4j/condition/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left > right ? left : right;
+        return result;
+    }
+    public static void main(String[] args) {
+        int result = Max.max(300,400);
+        System.out.println(result);
+    }
+}
