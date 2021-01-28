a0225e4aa251854a3506c94a4d3b965e62342694
USER <vidinvitalii@gmail.com>
2021-01-28 17:10:39 +0200

    new class DummyBot and class DummyBotTest

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+
+public class DummyBot {
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if (question.equals("Привет, Бот")) {
+            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+            rsl = "Привет умник";
+        } else if (question.equals("Пока.")) { /* заменить ... на проверку,
+        известен ли боту этот вопрос и он знает как на него ответить. */
+            /* заменить ... на правильный ответ rsl = "ответ по заданию". */
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+
+    public static void main(String[] args) {
+        String rsl = DummyBot.answer("Привет, Бот");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+        rsl = DummyBot.answer("Покаkjk.");
+        System.out.println(rsl);
+    }
+}
