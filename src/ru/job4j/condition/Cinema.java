56b0329a051a36412036b654555e94be88cd284d
USER <vidinvitalii@gmail.com>
2021-01-29 14:56:17 +0200

    metod permission

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class Cinema {
     public static void permission(boolean allowByParent, boolean hasMoney) {
         if (allowByParent && hasMoney) {
-            System.out.println("I can go to the cinema.");
+            System.out.println("I can go to the cinema");
         } else {
             System.out.println("I can't.");
         }
